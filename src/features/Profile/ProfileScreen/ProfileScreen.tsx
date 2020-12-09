3e9ddf256c170e42c2e51df0125c0bfdbcbb6e87
narvidas <m.narvidas@gmail.com>
2020-12-09 21:22:03 +0200

    Ensure user session is in-sync within Profile screen

--- src/features/Profile/ProfileScreen/ProfileScreen.tsx
+++ src/features/Profile/ProfileScreen/ProfileScreen.tsx
@@ -1,6 +1,6 @@
 import { NavigationProp } from "@react-navigation/native";
 import { Container, Content } from "native-base";
-import React, { FC, useContext } from "react";
+import React, { FC, useContext, useState } from "react";
 import FirebaseContext from "../../../config/firebaseContext";
 import { AnonymousView } from "./AnonymousView";
 import { AuthenticatedView } from "./AuthenticatedView";
@@ -12,7 +12,9 @@ interface Props {
 
 export const ProfileScreen: FC<Props> = ({ navigation }) => {
   const { auth } = useContext(FirebaseContext);
-  const loggedIn = !!auth.currentUser;
+  const [loggedIn, setLoggedIn] = useState<boolean>(!!auth.currentUser);
+
+  auth.onAuthStateChanged(user => (user ? setLoggedIn(true) : setLoggedIn(false)));
 
   return (
     <Container>
